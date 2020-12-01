d28c469541fc7f141962b7c58295ec95cbd6dccb
Lennart Fridén <lennart@devl.se>
2020-12-01 23:21:10 +0100

    Remove the default user during the secondary setup

--- boot/setup/init.sh
+++ boot/setup/init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+USER="devl"
+
 echo "Running init script"
 
 echo "[01] Setting the timezone"
@@ -8,17 +11,15 @@ sudo ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 echo "[02] Linking /etc/hostname to /boot/hostname"
 sudo rm -f /etc/hostname
 sudo ln -sf /boot/hostname /etc/hostname
+echo -e "\tAfter a reboot, this host will be known as '$(cat /etc/hostname)'"
 
 echo "[03] Linking /etc/hosts to /boot/hosts"
 sudo rm -f /etc/hosts
 sudo ln -sf /boot/hosts /etc/hosts
 
-echo "[04] Creating a new user"
-sudo adduser devl
-groups | sed 's/pi //g' | sed -e "s/ /,/g" | xargs -I{} sudo usermod -a -G {} devl
-
-echo "[05] Forcefully removing the default user"
-sudo deluser --force --remove-home pi
+echo "[04] Creating a new user called '${USER}'"
+sudo adduser $USER
+groups | sed 's/pi //g' | sed -e "s/ /,/g" | xargs -I{} sudo usermod -a -G {} $USER
 
 echo "[Done] Rebooting in 5 seconds. Please log in again as the new user and run the setup script"
 sleep 5
--- boot/setup/setup.sh
+++ boot/setup/setup.sh
@@ -3,40 +3,48 @@
 echo "Running setup script"
 cd $HOME
 
-echo "[01] Adding aliases"
-echo "ll='ls -lAF' >> $HOME/.bashrc"
+echo "[01] Forcefully removing the default user"
+sudo deluser --force --remove-home pi
 
-echo "[02] Installing the micro text editor"
+echo "[02] Adding aliases"
+cat << ALIASES >> $HOME/.bashrc
+ll='ls -lAF'
+ALIASES
+
+echo "[03] Installing the micro text editor"
 #sudo apt install xclip --yes
 curl https://getmic.ro | bash
 sudo mv micro /usr/bin
 sudo cp /boot/setup/.config/micro/settings.json $HOME/.config/micro/settings.json
+echo -e "\tTODO: Install and configure Micro plugins such as fzf."
 
-echo "[03] Updating system"
+echo "[04] Updating system"
 sudo apt update && sudo apt upgrade --yes
 
-echo "[04] Installing additional software"
+echo "[05] Installing additional software"
 sudo apt install automake autoconf curl git httpie libncurses5-dev libssl-dev --yes
 
-echo "[05] Installing the asdf version manager"
+echo "[06] Installing the asdf version manager"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
-echo "# asdf version manager" >> $HOME/.bashrc
-echo ". $HOME/.asdf/asdf.sh" >> $HOME/.bashrc
-echo ". $HOME/.asdf/completions/asdf.bash" >> $HOME/.bashrc
 . $HOME/.asdf/asdf.sh
 . $HOME/.asdf/completions/asdf.bash
+cat << ASDF >> $HOME/.bashrc
+# asdf version manager
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
+ASDF
 
-echo "[06] Installing Erlang"
+echo "[07] Installing Erlang"
 asdf plugin add erlang
 asdf install erlang latest
 asdf global erlang `asdf list erlang`
 
-echo "[07] Installing Elixir"
+echo "[08] Installing Elixir"
 asdf plugin add elixir
 asdf install elixir latest
 asdf global elixir `asdf list elixir`
 
-echo "[08] Installing Ruby"
+echo "[09] Installing Ruby"
 asdf plugin add ruby
 asdf install ruby latest
 asdf global ruby `asdf list ruby`
