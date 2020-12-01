bb9851bb59192a387adfcce64ed3f30e16beb016
Lennart Fridén <lennart@devl.se>
2020-12-01 23:19:55 +0100

    Generate a hosts file that properly sets the hostname

--- make-headless.sh
+++ make-headless.sh
@@ -5,22 +5,10 @@ read -p "Enter hostname: " hostname
 read -p "Enter SSID: " ssid
 read -p "Enter shared key: " psk
 
-echo "[01] Adding hostname to /boot/hostname and /boot/hosts"
-echo $hostname > /Volumes/boot/hostname
-cat << HOSTS > /Volumes/boot/hosts
-127.0.0.1       $hostname
-127.0.0.1       localhost
-::1             localhost ip6-localhost ip6-loopback
-fe00::0         ip6-localnet
-ff00::0         ip6-mcastprefix
-ff02::1         ip6-allnodes
-ff02::2         ip6-allrouters
-HOSTS
-
-echo "[02] Enable SSH on boot"
+echo "[01] Enable SSH on boot"
 touch /Volumes/boot/ssh
 
-echo "[03] Create /boot/wpa_supplicant.conf"
+echo "[02] Create /boot/wpa_supplicant.conf"
 cat << WIFI > /Volumes/boot/wpa_supplicant.conf
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
@@ -31,7 +19,7 @@ network={
 }
 WIFI
 
-echo "[04] Modifying /boot/config.txt"
+echo "[03] Modifying /boot/config.txt"
 cat << CONFIG >> /Volumes/boot/config.txt
 
 # Added by RaspUp
@@ -40,8 +28,20 @@ sdtv_mode=2
 hdmi_safe=1
 CONFIG
 
-echo "[05] Copy setup scripts to /boot/setup/"
+echo "[04] Copy setup scripts to /boot/setup/"
 cp -R boot/setup /Volumes/boot/
+
+echo "[05] Generating hostname and hosts files"
+echo $hostname > /Volumes/boot/setup/hostname
+cat << HOSTS > /Volumes/boot/setup/hosts
+127.0.0.1       localhost
+::1             localhost ip6-localhost ip6-loopback
+fe00::0         ip6-localnet
+ff00::0         ip6-mcastprefix
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+127.0.0.1       $hostname
+HOSTS
 
 echo "[Done] Unmounting the SD card"
 diskutil unmount /Volumes/boot
