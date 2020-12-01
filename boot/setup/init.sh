499494636de4cd13917e7626864d6e5add7f6274
Lennart Fridén <lennart@devl.se>
2020-12-01 18:57:17 +0100

    Set hostname and GPU memory

--- boot/setup/init.sh
+++ boot/setup/init.sh
@@ -5,18 +5,21 @@ echo "[01] Setting the timezone"
 sudo rm -f /etc/localtime
 sudo ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 
-echo "[02] Creating a new user"
+echo "[02] Linking the hostname to /boot/hostname"
+sudo ln -s /boot/hostname /etc/hostname
+
+echo "[03] Creating a new user"
 sudo adduser devl
 groups | sed 's/pi //g' | sed -e "s/ /,/g" | xargs -I{} sudo usermod -a -G {} devl
 
-echo "[03] Adding setup script to the new user's home directory"
+echo "[04] Adding setup script to the new user's home directory"
 sudo cp /boot/setup/setup.sh /home/devl/setup.sh
 sudo chown /home/devl/setup.sh devl
 sudo chmod +x /home/devl/setup.sh
 
-echo "[04] Forcefully removing the default user"
+echo "[05] Forcefully removing the default user"
 sudo deluser --force --remove-home pi
 
-echo "[05] Rebooting in 5 seconds. Please log in again as the new user and run the setup script"
+echo "[Done] Rebooting in 5 seconds. Please log in again as the new user and run the setup script"
 sleep 5
 sudo reboot
--- make-headless.sh
+++ make-headless.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
-# Enable SSH
-touch /Volumes/boot/ssh
-
-# Copy setup scripts to /boot/setup/
-cp -R boot/setup /Volumes/boot/
-
-# Create /boot/wpa_supplicant.conf
+echo "Welcome to RaspUp"
+read -p "Enter hostname: " hostname
 read -p "Enter SSID: " ssid
 read -p "Enter shared key: " psk
 
+echo "[01] Adding hostname to /boot/hostname and /boot/hosts"
+echo $hostname > /Volumes/boot/hostname
+cat << HOSTS > /Volumes/boot/hosts
+127.0.0.1       $hostname
+127.0.0.1       localhost
+::1             localhost ip6-localhost ip6-loopback
+fe00::0         ip6-localnet
+ff00::0         ip6-mcastprefix
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+HOSTS
+
+echo "[02] Enable SSH on boot"
+touch /Volumes/boot/ssh
+
+echo "[03] Create /boot/wpa_supplicant.conf"
 cat << WIFI > /Volumes/boot/wpa_supplicant.conf
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
@@ -20,5 +31,17 @@ network={
 }
 WIFI
 
-# Unmount the SD card
+echo "[04] Modifying /boot/config.txt"
+cat << CONFIG >> /Volumes/boot/config.txt
+
+# Added by RaspUp
+gpu_mem=16
+sdtv_mode=2
+hdmi_safe=1
+CONFIG
+
+echo "[05] Copy setup scripts to /boot/setup/"
+cp -R boot/setup /Volumes/boot/
+
+echo "[Done] Unmounting the SD card"
 diskutil unmount /Volumes/boot
