d9324bd25f4dfe4d03da29c2d2eda7a3fe4ac157
Lennart Fridén <lennart@devl.se>
2020-12-02 15:49:32 +0100

    Copy, don't link hostname and hosts files

--- boot/setup/init.sh
+++ boot/setup/init.sh
@@ -8,16 +8,12 @@ echo "[01] Setting the timezone"
 sudo rm -f /etc/localtime
 sudo ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 
-echo "[02] Linking /etc/hostname to /boot/hostname"
-sudo rm -f /etc/hostname
-sudo ln -sf /boot/etc/hostname /etc/hostname
+echo "[02] Setting hostname to '$(cat /boot/etc/hostname)'"
+sudo cp /boot/etc/hostname /etc/hostname
+sudo cp /boot/etc/hosts /etc/hosts
 echo -e "\tAfter a reboot, this host will be known as '$(cat /etc/hostname)'"
 
-echo "[03] Linking /etc/hosts to /boot/hosts"
-sudo rm -f /etc/hosts
-sudo ln -sf /boot/etc/hosts /etc/hosts
-
-echo "[04] Creating a new user called '${USER}'"
+echo "[03] Creating a new user called '${USER}'"
 sudo adduser $USER
 groups | sed 's/pi //g' | sed -e "s/ /,/g" | xargs -I{} sudo usermod -a -G {} $USER
 
