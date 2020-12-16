51144c7c511c60063b49aba8c07f3b61ddfb18b7
Lennart Fridén <lennart@devl.se>
2020-12-16 20:06:30 +0100

    Prefer uppercase variable names

--- raspup.sh
+++ raspup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-SDCARD=/Volumes/boot
+SDCARD="/Volumes/boot"
+BOOTHOME="$SDCARD/setup/home"
 
 echo "Welcome to RaspUp"
 if [ ! -d "$SDCARD" ]; then
@@ -9,16 +10,16 @@ if [ ! -d "$SDCARD" ]; then
 fi
 
 echo "[User]"
-read -p "Enter the username: " username
-read -sp "Enter the password: " password
+read -p "Enter the username: " USERNAME
+read -sp "Enter the password: " PASSWORD
 echo
 echo "[Network]"
-read -p "Enter hostname: " hostname
-read -p "Enter SSID: " ssid
-read -sp "Enter shared key (hidden): " psk
+read -p "Enter hostname: " HOSTNAME
+read -p "Enter SSID: " SSID
+read -sp "Enter shared key (hidden): " PSK
 echo
 echo "[Erlang/Elixir Cluster]"
-read -sp "Enter Erlang cookie (hidden): " erlangcookie
+read -sp "Enter Erlang cookie (hidden): " ERLANGCOOKIE
 
 echo "[01] Enable SSH on boot"
 touch $SDCARD/ssh
@@ -29,8 +30,8 @@ ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 country=SE
 network={
-  ssid="$ssid"
-  psk="$psk"
+  ssid="$SSID"
+  psk="$PSK"
 }
 WIFI
 
@@ -48,7 +49,7 @@ cp -R boot/setup $SDCARD/
 
 echo "[05] Generating hostname and hosts files"
 mkdir $SDCARD/etc
-echo $hostname > $SDCARD/etc/hostname
+echo $HOSTNAME > $SDCARD/etc/hostname
 cat << HOSTS > $SDCARD/etc/hosts
 127.0.0.1       localhost
 ::1             localhost ip6-localhost ip6-loopback
@@ -61,24 +62,24 @@ ff02::2         ip6-allrouters
 192.168.10.152  green.local
 192.168.10.153  blue.local
 192.168.10.160  neo.local
-127.0.0.1       $hostname
+127.0.0.1       $HOSTNAME
 HOSTS
 
-echo "[06] Removing raspberry.local and $hostname.local as known SSH hosts"
+echo "[06] Removing raspberrypi.local and $HOSTNAME.local as known SSH hosts"
 sed -i -e '/raspberrypi/d' ~/.ssh/known_hosts
-sed -i -e "/$hostname/d" ~/.ssh/known_hosts
+sed -i -e "/$HOSTNAME/d" ~/.ssh/known_hosts
 
 echo "[07] Copying public SSH keys to /boot/setup/home/authorized_keys"
-test -e ~/.ssh/id_rsa.pub && cat ~/.ssh/id_rsa.pub >> $SDCARD/setup/home/authorized_keys
-test -e ~/.ssh/id_ecdsa.pub && cat ~/.ssh/id_ecdsa.pub >> $SDCARD/setup/home/authorized_keys
-test -e ~/.ssh/id_ed25519.pub && cat ~/.ssh/id_ed25519.pub >> $SDCARD/setup/home/authorized_keys
+test -e ~/.ssh/id_rsa.pub && cat ~/.ssh/id_rsa.pub >> $BOOTHOME/authorized_keys
+test -e ~/.ssh/id_ecdsa.pub && cat ~/.ssh/id_ecdsa.pub >> $BOOTHOME/authorized_keys
+test -e ~/.ssh/id_ed25519.pub && cat ~/.ssh/id_ed25519.pub >> $BOOTHOME/authorized_keys
 
 echo "[08] Storing Erlang cookie"
-echo -n $erlangcookie > $SDCARD/setup/home/.erlang.cookie
+echo -n $ERLANGCOOKIE > $BOOTHOME/.erlang.cookie
 
 echo "[09] Storing user information"
-echo -n $username > $SDCARD/setup/username
-echo -n $password > $SDCARD/setup/password
+echo -n $USERNAME > $SDCARD/setup/username
+echo -n $PASSWORD > $SDCARD/setup/password
 
 echo "[Done] Unmounting the SD card"
 diskutil unmount $SDCARD
@@ -87,6 +88,6 @@ echo "[What now?]"
 echo "First, log in to the Pi and run the '/boot/setup/init.sh' script."
 echo "    ssh pi@raspberrypi.local"
 echo "Secondly, log in as the new user and run the '/boot/setup/setup.sh' script."
-echo "    ssh devl@$hostname.local"
+echo "    ssh $USERNAME@$HOSTNAME.local"
 
 
